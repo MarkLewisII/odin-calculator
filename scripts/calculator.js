947fdb8e76b3ca2ffd8863f5081dfeca0c25b3b5
MarkLewisII <mark.lewis070301@gmail.com>
2023-12-11 23:45:31 -0400

    Adds function to button that clears equation display and resets result

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -2,46 +2,53 @@ const equation = document.getElementById("display");
 const result = document.getElementById("resultField");
 
 function UpdateDisplay(buttonValue) {
-    equation.textContent += `${buttonValue}`;
+	equation.textContent += `${buttonValue}`;
 }
 
 function Operate() {
-    var equationText = equation.textContent.split(' ');
-    var num1 = equationText[0];
-    var operation = equationText[1];
-    var num2 = equationText[2];
-    
-    switch (operation) {
-        case '+':
-            result.textContent = Addition(num1, num2);
-            break;
-            case '-':
-                result.textContent = Subtraction(num1, num2);
-                break;
-                case 'x':
-            result.textContent = Multiplication(num1, num2);
-            break;
-        case '/':
-            result.textContent = Division(num1, num2);
-            break;
-        default:
-            alert("ERROR: Unsupported operation entered.");
-            break;
-    }
-    
-            console.log(`num1 = ${num1}\noperation = ${operation}\nnum2 = ${num2}\nresult = ${result}`);
+	var equationText = equation.textContent.split(" ");
+	var num1 = equationText[0];
+	var operation = equationText[1];
+	var num2 = equationText[2];
+
+	switch (operation) {
+		case "+":
+			result.textContent = Addition(num1, num2);
+			break;
+		case "-":
+			result.textContent = Subtraction(num1, num2);
+			break;
+		case "x":
+			result.textContent = Multiplication(num1, num2);
+			break;
+		case "/":
+			result.textContent = Division(num1, num2);
+			break;
+		default:
+			alert("ERROR: Unsupported operation entered.");
+			break;
+	}
+
+	console.log(
+		`num1 = ${num1}\noperation = ${operation}\nnum2 = ${num2}\nresult = ${result}`
+	);
 }
 
 function Addition(num1, num2) {
-    return parseInt(num1) + parseInt(num2);
+	return parseInt(num1) + parseInt(num2);
 }
 
 function Subtraction(num1, num2) {
-    return parseInt(num1) - parseInt(num2);
+	return parseInt(num1) - parseInt(num2);
 }
 function Multiplication(num1, num2) {
-    return parseInt(num1) * parseInt(num2);
+	return parseInt(num1) * parseInt(num2);
 }
 function Division(num1, num2) {
-    return parseInt(num1) / parseInt(num2);
+	return parseInt(num1) / parseInt(num2);
+}
+
+function Clear() {
+	equation.textContent = "";
+	result.textContent = "0";
 }
