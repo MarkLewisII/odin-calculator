5447d2aa40e19912f449b67bdec1be63df0490fb
MarkLewisII <mark.lewis070301@gmail.com>
2023-12-11 23:39:36 -0400

    Adds operations to evaluate the equation entered

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,41 +1,47 @@
-// const display = document.getElementById("display");
 const equation = document.getElementById("display");
+const result = document.getElementById("resultField");
 
 function UpdateDisplay(buttonValue) {
-    // display += `${buttonValue}`;
     equation.textContent += `${buttonValue}`;
-
-    console.log(equation);
-}
-
-function Display() {
-
 }
 
 function Operate() {
-
-	var [num1, operation, num2] = equation.split(" ");
-
-	switch (operation) {
-        case " + ":
-            Addition(num1, num2);
+    var equationText = equation.textContent.split(' ');
+    var num1 = equationText[0];
+    var operation = equationText[1];
+    var num2 = equationText[2];
+    
+    switch (operation) {
+        case '+':
+            result.textContent = Addition(num1, num2);
             break;
-        case ' - ':
-            Subtraction(num1, num2);
+            case '-':
+                result.textContent = Subtraction(num1, num2);
+                break;
+                case 'x':
+            result.textContent = Multiplication(num1, num2);
             break;
-        case ' x ':
-            Multiplication(num1, num2);
-            break;
-        case ' / ':
-            Division(num1, num2);
+        case '/':
+            result.textContent = Division(num1, num2);
             break;
         default:
             alert("ERROR: Unsupported operation entered.");
             break;
-	}
+    }
+    
+            console.log(`num1 = ${num1}\noperation = ${operation}\nnum2 = ${num2}\nresult = ${result}`);
 }
 
-function Addition(num1, num2) {}
-function Subtraction(num1, num2) {}
-function Multiplication(num1, num2) {}
-function Division(num1, num2) {}
+function Addition(num1, num2) {
+    return parseInt(num1) + parseInt(num2);
+}
+
+function Subtraction(num1, num2) {
+    return parseInt(num1) - parseInt(num2);
+}
+function Multiplication(num1, num2) {
+    return parseInt(num1) * parseInt(num2);
+}
+function Division(num1, num2) {
+    return parseInt(num1) / parseInt(num2);
+}
