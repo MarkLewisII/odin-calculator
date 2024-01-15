6e1647d91d5bf4eb3ad23fa9e0a8a6178cacce06
MarkLewisII <mark.lewis070301@gmail.com>
2024-01-15 16:04:01 -0400

    Removes attempt at multioperational equations

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -11,7 +11,91 @@ function Operate() {
 	var num1 = equationText[0];
 	var operation = equationText[1];
 	var num2 = equationText[2];
+	console.table(equationText);
+	
+	// for (let i = 1; i <= equationText.length-2; i + 2) {
+	// 	if (equationText[i + 1] == "+" || equationText[i + 1] == "-") {
+	// 		switch (equationText[i]) {
+	// 			case "+":
+	// 					result.textContent = Addition(
+	// 						equationText[0],
+	// 						equationText[i + 2]
+	// 					);
+	// 					break;
+	// 				case "-":
+	// 					result.textContent = Subtraction(
+	// 						equationText[0],
+	// 						equationText[i + 2]
+	// 					);
+	// 			default:
+	// 				alert("ERROR: Unsupported operation entered.");
+	// 				break;
+	// 		}
+	// 	}
+	// }
 
+	// for (let i = 1; i <= equationText.length-2; i + 2) {
+	// 	if((equationText[i + 1] == "x") || (equationText[i + 1] == "/")) {
+	// 		switch (equationText[i]) {
+	// 			case "x":
+	// 				result.textContent = Multiplication(
+	// 					equationText[0],
+	// 					equationText[i + 2]
+	// 				);
+	// 				break;
+	// 			case "/":
+	// 				if (equationText[0] == 0 || equationText[i + 2] == 0) {
+	// 					alert("MATH ERROR: Cannot divide by 0");
+	// 					Clear();
+	// 					break;
+	// 				}
+	// 				result.textContent = Division(
+	// 					equationText[0],
+	// 					equationText[i + 2]
+	// 				);
+	// 				break;
+	// 			default:
+	// 				alert("ERROR: Unsupported operation entered.");
+	// 				break;
+	// 		}
+	// 	}
+	// }
+
+	// switch (equationText[i + 1]) {
+	// 	case "+":
+	// 		result.textContent = Addition(
+	// 			equationText[0],
+	// 			equationText[i + 2]
+	// 		);
+	// 		break;
+	// 	case "-":
+	// 		result.textContent = Subtraction(
+	// 			equationText[0],
+	// 			equationText[i + 2]
+	// 		);
+	// 		break;
+	// 	case "x":
+	// 		result.textContent = Multiplication(
+	// 			equationText[0],
+	// 			equationText[i + 2]
+	// 		);
+	// 		break;
+	// 	case "/":
+	// 		if (equationText[0] == 0 || equationText[i + 2] == 0) {
+	// 			alert("MATH ERROR: Cannot divide by 0");
+	// 			Clear();
+	// 			break;
+	// 		}
+	// 		result.textContent = Division(
+	// 			equationText[0],
+	// 			equationText[i + 2]
+	// 		);
+	// 		break;
+	// 	default:
+	// 		alert("ERROR: Unsupported operation entered.");
+	// 		break;
+	// }
+	// }
 	switch (operation) {
 		case "+":
 			result.textContent = Addition(num1, num2);
@@ -23,7 +107,7 @@ function Operate() {
 			result.textContent = Multiplication(num1, num2);
 			break;
 		case "/":
-			if ((num1 == 0) || (num2 == 0)) {
+			if (num1 == 0 || num2 == 0) {
 				alert("MATH ERROR: Cannot divide by 0");
 				Clear();
 				break;
@@ -35,27 +119,34 @@ function Operate() {
 			break;
 	}
 
-	console.log(
-		`num1 = ${num1}\noperation = ${operation}\nnum2 = ${num2}\nresult = ${result}`
-	);
+	// console.log(
+	// 	`num1 = ${num1}\noperation = ${operation}\nnum2 = ${num2}\nresult = ${result}`
+	// );
 }
 
-function Addition(num1, num2) {
-	return parseInt(num1) + parseInt(num2);
-}
+// function Addition(num1, num2) {
+// 	return parseInt(num1) + parseInt(num2);
+// }
+let Addition = (num1, num2) => { return parseInt(num1) + parseInt(num2) };
 
-function Subtraction(num1, num2) {
+// function Subtraction(num1, num2) {
+// 	return parseInt(num1) - parseInt(num2);
+// }
+let Subtraction = (num1, num2) => {
 	return parseInt(num1) - parseInt(num2);
-}
-function Multiplication(num1, num2) {
+};
+// function Multiplication(num1, num2) {
+// 	return parseInt(num1) * parseInt(num2);
+// }
+let Multiplication = (num1, num2) => {
 	return parseInt(num1) * parseInt(num2);
-}
-function Division(num1, num2) {
-	// if ((num1 == 0) || (num2 == 0)) {
-	// 	return "MATH ERROR: Cannot divide by 0";
-	// }
+};
+// function Division(num1, num2) {
+// 	return parseInt(num1) / parseInt(num2);
+// }
+let Division = (num1, num2) => {
 	return parseInt(num1) / parseInt(num2);
-}
+};
 
 function Clear() {
 	equation.textContent = "";
